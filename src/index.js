09cc5729b6e09911111b469ce00e26e8e74e219c
sahib singh <ss0990100@gmail.com>
2024-06-01 11:31:41 +0530

    updating the controller for fetching from 3rd party

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const APIroutes = require("./routers/index");
 
 const PORT = 9876
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(APIroutes);
 
 app.listen(PORT,async ()=>{
     
