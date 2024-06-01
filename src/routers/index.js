25739465bb5256c387110e54cb162df637eef20d
sahib singh <ss0990100@gmail.com>
2024-06-01 11:09:16 +0530

    added routes and controller

---
+++ src/routers/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const endPointRoutes = require("./endPoints");
+
+router.use('number',endPointRoutes);
+
+module.exports = router;
